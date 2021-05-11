2bac74c6c12f552ebdf6a064b3a10fc4da33043a
macgyver1985 <felipe.augusto.franca@gmail.com>
2021-05-11 00:47:57 -0300

    Ajuste no autoMapper usado pela camada application.

--- src/application.layer/helper/autoMapper.ts
+++ src/application.layer/helper/autoMapper.ts
@@ -1,7 +1,11 @@
 import { AutoMapper } from '@layer/crossCutting/autoMapper';
 import { IAutoMapper } from '@layer/crossCutting/autoMapper/interfaces';
-import { EBusinessType, PricingInfosData, RealEstateData } from '@layer/domain/realEstate';
-import { PricingInfosDTO, RealEstateDTO } from '../models/realEstate';
+import {
+  AddressData, EBusinessType, GeoLocationData, LocationData, PricingInfosData, RealEstateData,
+} from '@layer/domain/realEstate';
+import {
+  AddressDTO, GeoLocationDTO, LocationDTO, PricingInfosDTO, RealEstateDTO,
+} from '../models/realEstate';
 
 const autoMapper: IAutoMapper = new AutoMapper();
 
@@ -11,6 +15,20 @@ autoMapper.createMap<RealEstateDTO, RealEstateData>(
 )
   .forMember('createdAt', (t) => new Date(t.createdAt))
   .forMember('updatedAt', (t) => new Date(t.updatedAt))
+  .forMember('id', (t) => t.id)
+  .forMember('bathrooms', (t) => t.bathrooms)
+  .forMember('bedrooms', (t) => t.bedrooms)
+  .forMember('images', (t) => t.images)
+  .forMember('listingStatus', (t) => t.listingStatus)
+  .forMember('listingType', (t) => t.listingType)
+  .forMember('owner', (t) => t.owner)
+  .forMember('parkingSpaces', (t) => t.parkingSpaces)
+  .forMember('usableAreas', (t) => t.usableAreas)
+  .forMember('address', (t) => autoMapper.mapper<AddressDTO, AddressData>(
+    Symbol.for('AddressDTO'),
+    Symbol.for('AddressData'),
+  )
+    .map(t.address, <AddressData>{}))
   .forMember('pricingInfos', (t) => autoMapper.mapper<PricingInfosDTO, PricingInfosData>(
     Symbol.for('PricingInfosDTO'),
     Symbol.for('PricingInfosData'),
@@ -21,9 +39,39 @@ autoMapper.createMap<PricingInfosDTO, PricingInfosData>(
   Symbol.for('PricingInfosDTO'),
   Symbol.for('PricingInfosData'),
 )
-  .forMember('businessType', (t) => EBusinessType[t.businessType])
+  .forMember('businessType', (t) => EBusinessType[t.businessType.toLowerCase()])
   .forMember('monthlyCondoFee', (t) => Number(t.monthlyCondoFee))
   .forMember('price', (t) => Number(t.price))
   .forMember('yearlyIptu', (t) => Number(t.yearlyIptu));
+
+autoMapper.createMap<AddressDTO, AddressData>(
+  Symbol.for('AddressDTO'),
+  Symbol.for('AddressData'),
+)
+  .forMember('city', (t) => t.city)
+  .forMember('neighborhood', (t) => t.neighborhood)
+  .forMember('geoLocation', (t) => autoMapper.mapper<GeoLocationDTO, GeoLocationData>(
+    Symbol.for('GeoLocationDTO'),
+    Symbol.for('GeoLocationData'),
+  )
+    .map(t.geoLocation, <GeoLocationData>{}));
+
+autoMapper.createMap<GeoLocationDTO, GeoLocationData>(
+  Symbol.for('GeoLocationDTO'),
+  Symbol.for('GeoLocationData'),
+)
+  .forMember('precision', (t) => t.precision)
+  .forMember('location', (t) => autoMapper.mapper<LocationDTO, LocationData>(
+    Symbol.for('LocationDTO'),
+    Symbol.for('LocationData'),
+  )
+    .map(t.location, <LocationData>{}));
+
+autoMapper.createMap<LocationDTO, LocationData>(
+  Symbol.for('LocationDTO'),
+  Symbol.for('LocationData'),
+)
+  .forMember('lat', (t) => t.lat)
+  .forMember('lon', (t) => t.lon);
 
 export default autoMapper;
