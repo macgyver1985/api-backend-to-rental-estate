e630b1a2d78b327405226c7c05921b39360bbbff
macgyver1985 <felipe.augusto.franca@gmail.com>
2021-05-12 11:28:12 -0300

    Inclusão de teste unitário para ObtainRealEstateController na camada presentations.

---
+++ src/presentations.layer/_tests/controllers/ObtainRealEstateController.spec.ts
@@ -0,0 +1,44 @@
+import CacheInFile from '@layer/adapters/cache/CacheInFile';
+import PartnerRepository from '@layer/adapters/repositories';
+import ServiceToObtainRealEstate from '@layer/adapters/services';
+import { ICache } from '@layer/application/interfaces/sockets/cache';
+import { IPartnerRepository } from '@layer/application/interfaces/sockets/repositories';
+import { IServiceToObtainRealEstate } from '@layer/application/interfaces/sockets/services';
+import ObtainListOfRealEstateHandler from '@layer/application/manageRealEstate/handlers';
+import { ICompatibilityFactory, IObtainListOfRealEstateHandler } from '@layer/application/manageRealEstate/interfaces';
+import CompatibilityFactory from '@layer/application/manageRealEstate/support';
+import { ContractValidator } from '@layer/crossCutting/fluentValidation';
+import { IContractValidator } from '@layer/crossCutting/fluentValidation/interfaces';
+import ObtainRealEstateController from '@layer/presentations/controllers';
+import { IObtainRealEstateController } from '@layer/presentations/interfaces/controllers';
+
+describe('Test ObtainRealEstateController', () => {
+  const cache: ICache = new CacheInFile();
+  const service: IServiceToObtainRealEstate = new ServiceToObtainRealEstate(cache);
+  const repository: IPartnerRepository = new PartnerRepository();
+  const compatibilityFactory: ICompatibilityFactory = new CompatibilityFactory();
+  const contractValidator: IContractValidator = new ContractValidator();
+  const handler: IObtainListOfRealEstateHandler = new ObtainListOfRealEstateHandler(
+    service,
+    repository,
+    compatibilityFactory,
+    contractValidator,
+    cache,
+  );
+  const controller: IObtainRealEstateController = new ObtainRealEstateController(
+    handler,
+    contractValidator,
+  );
+
+  it('Obbtem os dados com sucesso', async () => {
+    const result = await controller
+      .handle({
+        body: {
+          pageNumber: 1,
+          pageSize: 1,
+        },
+      });
+
+    expect(result).toEqual(true);
+  });
+});
